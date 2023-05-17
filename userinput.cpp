addcedc1a9812b417157138416fcb8f4bbe49917
FahadHasan8386 <fh223430@gmail.com>
2023-05-18 02:46:38 +0600

    User Input

---
+++ userinput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ;
+int main ()
+{
+    int a;
+    int b;
+    //user input
+    cin >> a;
+    cin >> b;
+    int sum = a + b;
+    cout<<"Sum = " << sum << endl;
+}
