a9b32f2876ad5f40d8e5c2bc2f56d26ad3703a92
FahadHasan8386 <fh223430@gmail.com>
2023-05-19 00:26:02 +0600

    Multiplication

---
+++ Multiplication.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int x , y;
+        cin >> x;
+        cin >> y;
+    int multiplication = x * y;
+        cout << "Multiplication = " << multiplication << endl;
+}
