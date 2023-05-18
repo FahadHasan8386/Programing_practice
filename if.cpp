049d75b9df8a0fd5cf2fd705d5236b83c929e985
FahadHasan8386 <fh223430@gmail.com>
2023-05-19 00:54:10 +0600

    If statement

---
+++ if.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int num;
+        cin >> num;
+        cout << "Enter a number :" << num << endl;
+
+    if (num > 0){
+        cout << "number is positive = " << num << endl;
+    }
+    if (num < 0){
+        cout << "number is negative = " << num << endl;
+    }
+    if (num == 0){
+        cout << "number is zero = " << num << endl;
+    }
+
+
+
+}
