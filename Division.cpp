d1062a28316c3d8e5337a5cfafa6b6ca6fed9794
FahadHasan8386 <fh223430@gmail.com>
2023-05-19 00:38:36 +0600

    Division

---
+++ Division.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a , b;
+        cin >> a >> b;
+    int division = (a + b) / 2;
+        cout << "Division = " << division << endl;
+}
