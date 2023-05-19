0e3c52408615dc5f2d183298185becc681bf3229
FahadHasan8386 <fh223430@gmail.com>
2023-05-20 00:56:31 +0600

    Leap-Year

---
+++ Leap year.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int year;
+        cout <<"Enter a year :";
+        cin >> year;
+    if (year % 4 == 0 && year % 100 != 0){
+        cout <<"Leap year ";
+    }
+    else if (year % 400 == 0){
+        cout <<"Leap year ";
+    }
+    else{
+        cout <<"not a leap year";
+    }
+}
