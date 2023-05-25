3c837ab4eaaeb453243a00738af31a6579a62dc7
FahadHasan8386 <fh223430@gmail.com>
2023-05-26 02:43:09 +0600

    Arr

---
+++ Arr.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int main [5] = {2 , 3, 4, 5, 6};
+
+        for (int i=0 ; i<5 ;i++)
+        {
+            cout << main [i]<<" "<<endl;
+        }
+}
