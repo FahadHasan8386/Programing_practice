5644d83fb5c2ff78aface3da2d239fb15baf657a
FahadHasan8386 <fh223430@gmail.com>
2023-06-09 23:25:02 +0600

    String Input

---
+++ String Input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    char name[30];
+        cout <<"Enter your name : "<<name;
+        cin >> name;
+
+        cout << "Welcome "<<name;
+
+}
