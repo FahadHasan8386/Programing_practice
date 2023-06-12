3c1014983dd708a8efbde4f327e22a8c516536c9
FahadHasan8386 <fh223430@gmail.com>
2023-06-13 01:25:32 +0600

    Function

---
+++ function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+    int x =30;
+
+void display()
+{
+        cout <<  "X = " << x + 10 << endl;
+}
+int main ()
+{
+        cout << "X = " << x << endl;
+        display ();
+}
