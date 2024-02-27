1615bf8d389be1dd62167f3429bbe4bfd74cf85b
fahadhasan <hasan.fahad67@outlook.com>
2024-02-27 18:44:19 +0600

    STL String

---
+++ STL String/StlString.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+
+    string s = "Fahad";
+
+    s += ' ';
+    s = s + 'a';//concatanation
+    s += 'b';
+    s += 'c';
+    //Size of String
+    cout << s.size() << endl;
+    //Single String output
+    cout << s[0] << " " << s[8] << " "<< endl;
+
+    cout << s << endl;
+}
