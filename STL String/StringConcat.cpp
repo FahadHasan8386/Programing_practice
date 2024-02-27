fc699d65a38f15093d75e31ddd40e264e0a1fb36
fahadhasan <hasan.fahad67@outlook.com>
2024-02-27 19:02:38 +0600

    String concatenate with char

---
+++ STL String/StringConcat.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main ()
+{
+    string f;
+    string s = "Fahad" , s1 = "Hasan";
+    char c = ' ';
+
+    f = s + c + s1;
+
+    s += c;
+    s += s1;
+
+
+
+        cout << f << endl;
+        cout << s;
+
+}
