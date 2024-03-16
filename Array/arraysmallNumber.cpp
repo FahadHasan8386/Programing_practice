d9edabc8d7692c90db1d6c6bc09f55c53bd594b1
fahadhasan <hasan.fahad67@outlook.com>
2024-03-16 14:52:57 +0600

    Small number in array

---
+++ Array/arraysmallNumber.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int num [3];
+    for (int i =0 ;i < 3 ; i++)
+    {
+        cin >> num[i];
+    }
+
+
+    sort (num,num + 3);
+        cout << num[0];
+
+}
