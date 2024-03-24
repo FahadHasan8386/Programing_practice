83d4a20189c2d68b311412b0c08769b92e59365e
fahadhasan <hasan.fahad67@outlook.com>
2024-03-24 23:59:28 +0600

    Linked 3rd Node

---
+++ Linked List/3rdnode.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+    int data;
+    struct node *link;
+};
+
+int main() {
+    struct node *head = new struct node;
+    head->data = 45;
+    head->link = NULL;
+
+    struct node *current = new struct node;
+    current->data = 98;
+    current->link = NULL;
+    head->link = current;
+
+    current = new struct node;
+    current->data = 3;
+    current->link = NULL;
+
+    head->link->link = current;
+    return 0;
+}
