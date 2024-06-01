69060e6289d24cc5c6c18d69f74bf42177cf34a0
fahadhasan <hasan.fahad67@outlook.com>
2024-06-01 12:59:45 +0600

    Mid string erase

---
+++ STL String/midstring_erase.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main ()
+{
+    string s = "FahadHasan";
+    //remove 2 -5 index value
+    s.erase(s.begin()+2 , s.begin()+5);
+
+    cout << s << endl;
+}
