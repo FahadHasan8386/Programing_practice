1c010bf7291e524c8a3d825ea7c409387a486f46
fahadhasan <hasan.fahad67@outlook.com>
2024-06-01 12:52:42 +0600

    String is upper ,lower ,toupper

---
+++ STL String/isupper.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main ()
+{
+    char c = 'a';
+    char c1 = ' ';
+    bool f1 = isupper(c);
+    bool f2 = islower(c);
+    bool f3 = isspace(c1);
+
+    cout << f1 << endl;
+    cout << f2 << endl;
+    cout << f3 << endl;
+
+}
