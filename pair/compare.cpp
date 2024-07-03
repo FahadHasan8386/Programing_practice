4f2dc3323f0d0e02be517a1831b60c170ab18d50
fahadhasan <hasan.fahad67@outlook.com>
2024-07-03 12:15:21 +0600

    unique sort in pair

--- pair/compare.cpp
+++ pair/compare.cpp
@@ -10,11 +10,21 @@ int main ()
     v.push_back({"zvi" , 13});
     v.push_back({"Imran" , 77});
     v.push_back({"Sadman" , 55});
+    v.push_back({"Fahad" , 33});
+    v.push_back({"Fahad" , 33});
 
     sort(v.begin() ,v.end());
 
     for(auto u : v )
     {
         cout << u.first << " " <<u.second <<endl;
+    }
+
+    cout << endl;
+    int sz = unique(v.begin(),v.end()) -v.begin();
+
+    for (int i = 0 ; i < sz ; i++ )
+    {
+        cout << v[i].first << " " <<v[i].second <<endl;
     }
 }
