43762897978d3c4432e7bb33e77dc2d3bd44d2ea
Fahad Hasan <hasan.fahad67@outlook.com>
2024-09-10 14:28:44 +0600

    Geeks For geeks

--- Stack/stack2.cpp
+++ Stack/stack2.cpp
@@ -6,42 +6,41 @@ bool isbalanced(char c1, char c2) {
 }
 
 int main() {
-    int t ;
+    int t;
     cin >> t;
 
-    string s;
-    cin >> s;
+    while (t--) {
+        string s;
+        cin >> s;
+        stack<char> st;
+        bool done = true;
 
-    stack<char> st;
-    bool done = true;
-    while(t--)
-    {
         for (auto u : s) {
-        if (u == '(' || u == '{' || u == '[') {
-            st.push(u);
-        } else {
-            if (st.empty()) {
-                done = false;
-                break;
+            if (u == '(' || u == '{' || u == '[') {
+                st.push(u);
             } else {
-                if (isbalanced(st.top(), u)) {
-                    st.pop();
-                } else {
+                if (st.empty()) {
                     done = false;
                     break;
+                } else {
+                    if (isbalanced(st.top(), u)) {
+                        st.pop();
+                    } else {
+                        done = false;
+                        break;
+                    }
                 }
             }
         }
-    }
-    if (!st.empty()) done = false;
-    if (done) {
-        cout << "YES" << endl;
-    } else {
-        cout << "NO" << endl;
+
+        if (!st.empty()) done = false;
+
+        if (done) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
     }
 
     return 0;
-
-    }
-
 }
